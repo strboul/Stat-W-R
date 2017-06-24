ba31f398a6f7c1c1e6a67e92cdf4bfcca9885195
strboul <strboul@users.noreply.github.com>
2017-06-25 00:34:16 +0200

    Add files via upload

---
+++ libraries.R
@@ -0,0 +1,15 @@
+### INSTALL/UPDATE PACKAGES ##
+# install.packages("tidyverse")
+
+
+### LOAD LIBRARIES ###
+library(tidyverse) #
+# (Imports: broom, dplyr, forcats, ggplot2, haven,
+# httr, hms, jsonlite, lubridate, magrittr, modelr,
+# purrr, readr, readxl, stringr, tibble, rvest, tidyr, xml2)
+library(data.table)
+library(RColorBrewer)
+library(car) # For recode variables reversely
+library(psych)
+library(matrixStats) # High-performing functions operating on rows and columns of matrices
+library(colorspace) # For ggplot colors
