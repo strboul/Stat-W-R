59eb167fee832fb1ccfb44a4a4a49ca496508479
strboul <strboul@users.noreply.github.com>
2017-06-26 01:06:12 +0200

    Update help.R

--- basics/help.R
+++ basics/help.R
@@ -1,2 +1,10 @@
 ?mean                  #opens the help page for the mean function
 ??plotting             #searches for topics containing words like "plotting"
+
+help("mean")
+help.search("plotting")
+help.search("regression model")
+
+# The apropos function[5] finds variables (including functions)
+# that match its input. This is really useful if you can only half-remember the name of a variable
+apropos("vector")
