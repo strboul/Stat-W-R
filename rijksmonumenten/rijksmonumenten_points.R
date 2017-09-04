7adfed2d8147deb3f42ed1074f7e0c1845111fb4
strboul <strboul@users.noreply.github.com>
2017-09-04 20:29:34 +0300

    Create rijksmonumenten_points.R

---
+++ rijksmonumenten/rijksmonumenten_points.R
@@ -0,0 +1,6 @@
+#Rijksmonumenten points map
+read.csv(...)
+points(examine4[,1:2], col="orange", pch=20, cex=0.1) #First 1st and 2nd columns and all rows with lat and long values.
+plot(nl01)
+grid()
+box()
