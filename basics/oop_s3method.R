971e3a6918ce528f5f8d3e0638c8dbb3da41a158
strboul <strboul@users.noreply.github.com>
2017-12-22 22:44:09 +0100

    Create oop_s3method.R

---
+++ basics/oop_s3method.R
@@ -0,0 +1,27 @@
+#S3 methods
+
+methods(print)
+# [1] print.acf*                                        
+# [2] print.all_vars*                                   
+# [3] print.anova*                                      
+# [4] print.anova.lme*                                  
+# [5] print.any_vars*                                   
+# [6] print.aov*                                        
+# [7] print.aovlist*                                    
+# [8] print.ar*                                         
+# [9] print.Arima*                                      
+# [10] print.arima0*                                     
+
+methods(summary)
+
+find("dist")
+[1] "package:stats"
+
+z <- data.frame(cbind(x,y))
+class(z)
+
+summary.data.frame(z)
+summary.aov(z)
+
+summary(z)
+table(z$x, z$y)
